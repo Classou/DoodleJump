204142f30f3d20e6f12812d0339c09f6e4ac9a32
showsad <antoine.chaussade@eleves.enpc.fr>
2017-04-28 14:27:14 +0200

    petites modifs

--- jumper.h
+++ jumper.h
@@ -1,16 +1,24 @@
 #ifndef JUMPER_H
 #define JUMPER_H
+#include <Imagine/Images.h>
+#include <Imagine/Graphics.h>
+using namespace Imagine;
 #include <vector>
+#include <list>
+#include <iostream>
+#include <cassert>
+using namespace std;
+const float dt=0.1;
 class jumper{
-    int x;\\position du sauteur
-    int z; \\altitude du sauteur par rapport au milieu de l écran
-    float m; \\masse du sauteur
-    vector v; \\vitesse du sauteur
-    float a; \\accélération du sauteur
+    int x;//position du sauteur
+    int y; //altitude du sauteur par rapport au milieu de l écran
+    float m; //masse du sauteur
+    IntPoint2 v; //vitesse du sauteur
+    IntPoint2 a; //accélération du sauteur
     public :
         void accelere();
         void bouge();
-        void test_rebond(marche [])
+        void test_rebond(marche []);
 };
 
 
