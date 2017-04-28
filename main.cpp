1aadbf9496f78ce5373bf6ee812ce90752005ade
Classou <Clarisse@MacBook-Clarisse.local>
2017-04-28 15:51:23 +0200

    Déplacement de la fonction de déplacement de l’environnement vers une fonction annexe

--- main.cpp
+++ main.cpp
@@ -7,6 +7,30 @@ using namespace std;
 
 
 const int dt=0.1;//temps infinitésimal
+
+
+void deplacement_environnement(std :: vector<marche>&Marches){
+    if(((Marches[0]).posCoin()).y()>8* height_plat){
+        marche a(width_plat,height_plat,col);
+        Marches.insert(Marches.begin(),a);
+    }
+
+    for (int i=0;i<Marches.size()-1;i++){
+        Marches[i].efface();
+        Marches[i].defile();
+        Marches[i].affiche();
+    }
+    if(Marches.back().posCoin().y()+height_plat>=height_window){
+        Marches.back().efface();
+        Marches.pop_back();
+    }
+    else{
+        Marches.back().efface();
+        Marches.back().defile();
+        Marches.back().affiche();
+    }
+}
+
 //======================================
 int main(){
     openWindow(width_window,height_window);
@@ -17,24 +41,8 @@ int main(){
     Marches.push_back(b);
 
     while(pas_perdu){
-
-        if(((Marches[0]).posCoin()).y()>4* height_plat){
-            marche a(width_plat,height_plat,col);
-            Marches.insert(Marches.begin(),a);
-        }
-
-        for (int i=0;i<Marches.size()-1;i++){
-            Marches[i].defile();
-            Marches[i].affiche();
-        }
-        if(Marches.back().posCoin().y()+height_plat>=height_window){
-            Marches.pop_back();
-        }
-        else{
-            Marches.back().defile();
-            Marches.back().affiche();
-        }
-        milliSleep(100);
+        deplacement_environnement(Marches);
+        milliSleep(10);
     }
 
 
