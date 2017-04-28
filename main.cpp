cde3614a386216230ac311c6c03ee37de57aa48d
BibiOnGit <davidsu242@gmail.com>
2017-04-28 14:42:42 +0200

    Changement main, pour faire defiler les marches

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <ctime>
 using namespace std;
 #include "marche.h"
+#include <iostream>
+#include<list>
 
 
 
@@ -9,18 +11,29 @@ using namespace std;
 int main(){
     openWindow(width_window,height_window);
 
+    std :: list<marche> Marches;
+    bool pas_perdu=true;
 
-    //Création d'une liste de marches
-    int nb_marches=0;
-    marche* marches_toutes=new marche[100];
-    for(int i=0;i<5;i++){
-        marche M(width_plat,height_plat,col);
-        nb_marches+=1;
-        marches_toutes[i]=M;
-    }
-    for(int i=0;i<nb_marches;i++){
-        marches_toutes[i].affiche();
-        click();
+
+    while(pas_perdu){
+
+        if(Marches[Marches.begin()]>4*height_plat){
+            marche a(width_plat,height_plat,col);
+            Marches.push_front(a);
+        }
+
+        for (int i=0;i<Marches.size()-1;i++){
+            Marches[i].defile();
+            Marches[i].affiche();
+        }
+        if(Marches[Marches.size()].posCoin().y+height_plat>=height_window){
+            Marches.pop_back();
+        }
+        else{
+            Marches[Marches.size()].defile();
+            Marches[Marches.size()].affiche();
+        }
+
     }
 
 
